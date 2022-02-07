b46aa8c386012bc37a65f92a78acd9c890d49977
deniskurochkin <deniskurochkin@ozon.ru>
2022-02-07 18:24:01 +0300

    feat: dragging into columns

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import {Box, Grid, Paper, styled} from "@mui/material";
 
@@ -10,18 +10,61 @@ const Item = styled(Paper)(({ theme }) => ({
   color: theme.palette.text.secondary,
 }));
 
-function App() {
+const data = [
+  [1, 2, 3],
+  [4, 5]
+]
+
+const App = () => {
+  const [draggingElement, setDraggingElement] = useState<HTMLDivElement | null>(null)
+
+  const handleContainerDragOver = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault()
+
+    const target = e.target as HTMLDivElement
+    const container = target.classList.contains('drag-in-column') ? target : target.closest('.drag-in-column')
+
+    if (container) {
+      container.appendChild(draggingElement!)
+    }
+  }
+
+  const handleItemDragStart = (e: React.DragEvent<HTMLDivElement>) => {
+    setDraggingElement(e.currentTarget)
+    e.currentTarget.style.opacity = '0.5'
+  }
+
+  const handleItemDragEnd = (e: React.DragEvent<HTMLDivElement>) => {
+    setDraggingElement(null)
+    e.currentTarget.style.opacity = '1'
+  }
+
   return (
-    <Box sx={{ flexGrow: 1, padding: 3, background: '#E7EBF0', minHeight: '100vh' }}>
-      <Grid container spacing={2}>
-        <Grid item xs={6}>
-          <Item>1</Item>
-          <Item>2</Item>
-          <Item>3</Item>
-        </Grid>
-        <Grid item xs={6}>
-          <Item>4</Item>
-        </Grid>
+    <Box sx={{ flexGrow: 1, padding: 3, background: '#E7EBF0', minHeight: '100vh', boxSizing: 'border-box' }}>
+      <Grid
+        spacing={2}
+        container
+        onDragOver={handleContainerDragOver}
+      >
+        {data.map((column, columnIndex) => (
+          <Grid
+            key={columnIndex}
+            xs={6}
+            className={'drag-in-column'}
+            item
+          >
+            {column.map((cell, cellIndex) => (
+              <Item
+                key={`${columnIndex}${cellIndex}`}
+                draggable
+                onDragStart={handleItemDragStart}
+                onDragEnd={handleItemDragEnd}
+              >
+                {cell}
+              </Item>
+            ))}
+          </Grid>
+        ))}
       </Grid>
     </Box>
   );
